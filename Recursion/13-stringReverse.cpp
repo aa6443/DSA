7f7bcda52536042a299eba5789a8bae5aef43aae
Aniket Kumar <aa6443@srmist.edu.in>
2024-08-15 17:34:29 +0530

    reversing a string using recursion

---
+++ Recursion/13-stringReverse.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void revStr(string &s,int low,int high){
+    //base case 
+    if(low>=high){
+        return ;
+    }
+    //processing 
+    swap(s[low],s[high]);
+
+    //recurrence relation
+    revStr(s,low+1,high-1);
+}
+
+int main() {
+    // Your code goes here
+    string s = "abcde";
+    cout<<"Original String: "<<s<<endl;
+    int low = 0;
+    int high = s.size()-1;
+    revStr(s,low,high);
+    cout<<"After recursion String: "<<s<<endl;
+    
+    return 0;
+}
