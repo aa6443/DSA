88fc3d5c2da51db29167970d088bf43d6d45fd93
Aniket Kumar <aa6443@srmist.edu.in>
2024-12-04 08:30:06 +0530

    TREE BASICS

--- Trees/02_level-order-traversal.cpp
+++ Trees/02_level-order-traversal.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//creating a class 
 class node{
     public:
     int data;
     node* left;
     node* right;
-    //creating a constructor 
+    //constructor 
     node(int d){
         this->data = d;
         this->left = NULL;
@@ -15,25 +14,24 @@ class node{
     }
 };
 node* buildTree(node* root){
-    cout<<"Enter the data : ";
+    cout<<"Enter the root value : ";
     int data;
     cin>>data;
     root = new node(data);
-    //if we get -1 it means null vale 
-    if(data == -1 ){
-        return NULL;
-    }
-    cout<<"Enter the data in left of : "<<data<<endl;
+
+    cout<<"Enter the value left of : "<<data<<endl;
     root->left = buildTree(root->left);
-    cout<<"Enter the data in right of : "<<data<<endl;
+    cout<<"Enter the value right of : "<<data<<endl;
     root->right = buildTree(root->right);
     return root;
 }
-int main(){
 
-    node* root = NULL;
+void levelOrderTraversal(){
     
-    root = buildTree(root);
+}
 
+int main(){
+    node* root = NULL;
+    root = buildTree(root);
     return 0;
 }
